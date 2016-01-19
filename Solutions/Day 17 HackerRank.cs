0dc8f77692d63d2e632fc8b2b83d7fa0738d3c3a
Juan Carlos <el2022@hotmail.com>
2016-01-18 23:39:14 -0400

    Create Day 17 HackerRank.cs
    handling exceptions

---
+++ Solutions/Day 17 HackerRank.cs
@@ -0,0 +1,15 @@
+//Write your code here
+class Calculator
+    {
+
+        public int power(int n, int p)
+        {
+            if (n < 0 || p < 0)
+                throw new Exception("n and p should be non-negative");
+
+            else
+                return (int)Math.Pow(n, p);
+
+        }
+
+    }
